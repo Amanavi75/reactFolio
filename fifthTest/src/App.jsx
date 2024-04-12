25b49e83cd88cf4a439e2aba5508bb208733b118
amanavi75 <amanavi75@gmail.com>
2024-04-12 23:37:31 +0530

    added required components

--- fifthTest/src/App.jsx
+++ fifthTest/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Testimonials } from './components/Testimonials'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -15,7 +15,7 @@ function App() {
         underline
       </div>
       <div>
-        <Testimonial/>
+        <Testimonials/>
       </div>
     </div>
   )
