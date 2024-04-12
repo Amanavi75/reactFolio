857cbe28e55dcfe79267c7b65536393d81ec7f2c
amanavi75 <amanavi75@gmail.com>
2024-04-13 00:23:07 +0530

    donw with the testamonials project

--- fifthTest/src/App.jsx
+++ fifthTest/src/App.jsx
@@ -9,15 +9,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div>
-        <h1> Our testimonials </h1>
-      </div>
-      <div>
-        underline
-      </div>
-      <div>
-        <Testimonials reviews = {reviews} />
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonial</h1>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonials reviews = {reviews}/>
       </div>
     </div>
   )
