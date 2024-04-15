4af93eb7a2173626b6f02e3730a00d5ad6e88d3b
amanavi75 <amanavi75@gmail.com>
2024-04-15 14:33:26 +0530

    started with react forms

---
+++ forms/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  function changeHandler ( event){
+    const[formData,setFormData] = useState({firstName:"", lastName:"",pass:""})
+    setFormData(prevFormdata=>{
+     return {
+      ...prevFormdata , 
+      [event.target.name] :event.target.value   
+      
+      // here we have name property which will save our previous state and further goes on 
+     }
+    })
+    console.log(event.target.name);
+    }
+
+   
+
+  return (
+    <div>
+      <form action="">
+        <input type="text"
+         placeholder='firstName'
+         onChange={changeHandler}
+         firstName='firstName'
+          />
+        <input type="text"
+         placeholder='LastName'
+         onChange={changeHandler}
+         name='lastName'
+          />
+        <input type="text"
+         placeholder='pass'
+         onChange={changeHandler}
+         name='password'
+          />
+      </form>
+
+      
+    </div>
+  )
+}
+
+export default App
