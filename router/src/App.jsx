0d6d2991b836a18e06e03e21140a1adf4e665763
amanavi75 <amanavi75@gmail.com>
2024-04-18 17:33:03 +0530

    added react routers

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Route,Routes} from 
+"react-router-dom"
+import Home from './Components/Home'
+import Support from './Components/Support'
+import About from './Components/About'
+import Labs from './Components/Labs'
+import Notfound from './Components/Notfound'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      app
+     <div>
+      <Routes>
+        <Route path="/" element={<div> home page</div>} />
+        <Route path="/support" element={<Support/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/labs" element={<Labs/>} />
+        <Route path="*" element={<Notfound/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+     </div>
     </>
   )
 }
---
+++ router/src/Components/Notfound.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const notfound = () => {
+  return (
+    <div>notfound</div>
+  )
+}
+export default notfound;
