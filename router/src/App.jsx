50595635bfc5fe7688c5d7f8061e24b062aee9a1
amanavi75 <amanavi75@gmail.com>
2024-04-19 23:14:35 +0530

    added  useNavigate hook and navLink

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Route,Routes} from 
+import {Link, Route,Routes} from 
 "react-router-dom"
 import Home from './Components/Home'
 import Support from './Components/Support'
@@ -16,6 +16,32 @@ function App() {
   return (
     <>
      <div>
+      <nav>
+
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/support'>Support</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/labs'>Labs</Link>
+            {/* link will automatically attach a class with name "active" */}
+          </li> 
+        </ul>
+      </nav>
+
+
+
+
+
+
+
+
       <Routes>
         <Route path="/" element={<div> home page</div>} />
         <Route path="/support" element={<Support/>} />
@@ -30,3 +56,4 @@ function App() {
 }
 
 export default App
+
--- router/src/Components/Labs.jsx
+++ router/src/Components/Labs.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const labs = () => {
+  
+  const navigate = useNavigate();
+  // use to navigate directly to page 
+
+
+  function clickHandler(){
+    
+    navigate("/about")
+   
+
+  }
   return (
-    <div>labs</div>
+    <div>
+      labs
+
+      <button  onClick={clickHandler}> Move to About Page</button>
+      
+      </div>
+
   )
 }
 
